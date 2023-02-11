5982a83e03b14e6dcb2dd218e4cd854cedae8597
mizzuu <mizuho.tohma72@gmail.com>
2023-02-10 17:14:21 -0800

    add if statement

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,11 +3,16 @@ import React from "react";
 const Table = (props) => {
     return (
         <div className="mx-7">
-            {props.baby.map((animal) => (
-                <p key={animal.name}>
-                    {animal.name}: {animal.characteristics.name_of_young}
-                </p>
-            ))}
+            <p> {console.log(props.baby.length)} </p>
+            {props.baby.length > 0 ? (
+                props.baby.map((animal) => (
+                    <p key={animal.name}>
+                        {animal.name}: {animal.characteristics.name_of_young}
+                    </p>
+                ))
+            ) : (
+                <p>no</p>
+            )}
         </div>
     );
 };
