358ae7deec9c1708c23e97c1a14bd77355d29f66
mizzuu <mizuho.tohma72@gmail.com>
2023-02-19 16:28:22 -0800

    adjust if statement and styling

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,16 +3,18 @@ import React from "react";
 const Table = (props) => {
     return (
         <div className="mx-7">
-            <p> {console.log(props.baby.length)} </p>
             {props.baby.length > 0 ? (
                 props.baby.map((animal) => (
-                    <p key={animal.name}>
-                        {animal.name}: {animal.characteristics.name_of_young}
-                    </p>
+                    <div className="text-slate-100 flex">
+                        <p key={animal.name}>{animal.name}: </p>
+                        <p className="font-bold underline">
+                            {animal.characteristics.name_of_young}
+                        </p>
+                    </div>
                 ))
-            ) : (
-                <p>no</p>
-            )}
+            ) : props.addText ? (
+                <p className="text-slate-100">No Results</p>
+            ) : null}
         </div>
     );
 };
