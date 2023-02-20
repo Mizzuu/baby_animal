4bddf026d5f94c2b5282a25924fd8d888c44869e
mizzuu <mizuho.tohma72@gmail.com>
2023-02-19 16:27:10 -0800

    style bg color

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Input from "./components/Input";
 
 function App() {
     return (
-        <div className="font-mono bg-slate-600 h-full">
+        <div className="font-mono bg-slate-600 h-screen m-0 text-start">
             <Header />
             <Input />
         </div>
